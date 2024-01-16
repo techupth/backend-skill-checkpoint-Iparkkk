d0ea86079dc1fbf50e9955ad86593ca2adaa94d9
Park <tanagrit.sornchaiyagorn@gmail.com>
2024-01-16 12:55:16 +0700

    feature: add feature create question

--- router-group/question-posts.js
+++ router-group/question-posts.js
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
+import validateData from "../middldewares/validateData.js";
 
 const questionPostsRouter = Router();
 
@@ -6,4 +9,17 @@ questionPostsRouter.get("/", (req, res) => {
   return res.json("Hello worlds test database connected");
 });
 
+questionPostsRouter.post("/", validateData, async (req, res) => {
+  const collection = db.collection("question-posts");
+  const questionBlogData = { ...req.body };
+
+  try {
+    const createQuestion = await collection.insertOne(questionBlogData);
+    return res.json({
+      status: `201 question post ID: ${createQuestion.insertedId}has been create successfully`,
+    });
+  } catch {
+    return res.json({ status: "500 can't create new question post" });
+  }
+});
 export default questionPostsRouter;
